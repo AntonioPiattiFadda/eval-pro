8a2ae7a01c1748601df2f961791377bc02f88dcc
Antonio Piatti <antonio.piattifadda@gmail.com>
2026-04-05 23:49:59 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,7 @@ function RootRedirect() {
 
   if (lookingForSession || loading) return <LoadingScreen />
   if (!userSession) return <Navigate to="/login" replace />
+
   if (activeRole) return <Navigate to={ROLE_DASHBOARD[activeRole]} replace />
   return <LoadingScreen />
 }
